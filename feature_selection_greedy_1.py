58f87187e7a816de4f9fcb8ffbc21750bd7a2fba
Yash Gupta <34357926+erYash15@users.noreply.github.com>
2019-06-18 16:39:39 +0530

    Update feature_selection_greedy_1.py

--- feature_selection_greedy_1.py
+++ feature_selection_greedy_1.py
@@ -29,19 +29,19 @@ def loaddatasetMTS(dname):
     trainlabels = data['mts']['trainlabels'][()]
     test = data['mts']['test'][()]
     testlabels = data['mts']['testlabels'][()]
-    var = train[0].shape[0]
+    #var = train[0].shape[0]
     # normalize train data
-    for i in range(len(train)):
-        for v in range(var):
-            train[i]=train[i].astype('float64')
-            if np.std(train[i][v])!=0:
-                train[i][v] = stats.zscore(train[i][v])
+    #for i in range(len(train)):
+    #    for v in range(var):
+    #        train[i]=train[i].astype('float64')
+    #        if np.std(train[i][v])!=0:
+    #            train[i][v] = stats.zscore(train[i][v])
     # normalize test data
-    for i in range(len(test)):
-        for v in range(var):
-            test[i]=test[i].astype('float64')
-            if np.std(test[i][v])!=0:
-                test[i][v]= stats.zscore(test[i][v])
+    #for i in range(len(test)):
+    #    for v in range(var):
+    #        test[i]=test[i].astype('float64')
+    #        if np.std(test[i][v])!=0:
+    #            test[i][v]= stats.zscore(test[i][v])
     return train,trainlabels,test,testlabels
 
 train,trainlabels,test,testlabels = loaddatasetMTS("ECG")
@@ -63,28 +63,35 @@ def MIL(shapelet_local,X):
         if(dist<shapelet_local[2]):
             return i+len(shapelet_local[0])
     return len(X)
+#returns the MIL distance if fouund else total length
 	
 def earli_cal(shapelet_local,data):
     earliness = 0
+    count = 0
     for s in range(data.shape[0]):
-        earliness = earliness + (1-(MIL(shapelet_local,data[s][int(shapelet_local[5])])/len(data[s][int(shapelet_local[5])])))
-    earliness = earliness/data.shape[0]
+        early = MIL(shapelet_local,data[s][int(shapelet_local[5])])/len(data[s][int(shapelet_local[5])])
+        if(early<1):
+            earliness += (1 - early)
+            count += 1
+    earliness = earliness/count
     return earliness
-	
 
-###MODULE-3###	
+
+###MODULE-3###
 '''fuction of finding quality'''
 def feature_selection(shapelets_local,data,labels,w0=1,w1=1,w2=1):
     GEFM_lst = [[y for x in range(1)] for y in range(len(shapelets_local))]
-    imp_shapelets = []
+ #   imp_shapelets = []
     counter = 0
-    for shapelet_local in shapelets_local:
+    for shapelet_local in shapelets_local:#for each shapelet in all shaplets
+        print(counter)
         earliness = earli_cal(shapelet_local,data)
         #print(earliness,shapelet[3],shapelet[4])
         #print(earliness)
         if(earliness == 0 or shapelet_local[3]==0 or shapelet_local[4]==0):
             GEFM=0
         else:
+            #GEFM=2/((1/shapelet_local[3])+(1/shapelet_local[4]))
             GEFM=1/((w0/earliness)+(w1/shapelet_local[3])+(w2/shapelet_local[4]))
         GEFM_lst[counter].insert(0,GEFM)
         counter = counter + 1
